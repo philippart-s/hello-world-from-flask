c52c56df58f31c5853eaf9f264127a8d054016f9
philippart-s <wildagsx@gmail.com>
2023-01-31 08:06:38 +0000

    feat: ✨ add python emoji

--- hello-world.py
+++ hello-world.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>👋 Hello, World! 🌎</p>"
+    return "<p>👋 Hello, World! 🌎🐍</p>"
 
 if __name__ == '__main__':
     # starting app
